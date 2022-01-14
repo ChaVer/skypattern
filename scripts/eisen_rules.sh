eeb4dfc1f037b637668ed62d3dd2a794add19b80
Charles Vernerey <charlesvernerey2@gmail.com>
2022-01-14 22:05:15 +0100

    Add eisen scripts

---
+++ scripts/eisen_rules.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+java -cp target/skypattern-1-jar-with-dependencies.jar org.imt.skypattern.ar.experiments.ExperimentLaunch
+java -cp target/skypattern-1-jar-with-dependencies.jar org.imt.skypattern.ar.experiments.ExperimentLaunch2
---
+++ scripts/eisen_skypatterns.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+java -cp target/skypattern-1-jar-with-dependencies.jar org.imt.skypattern.closedsky.experiments.EisenExperiment
+java -cp target/skypattern-1-jar-with-dependencies.jar org.imt.skypattern.closedsky.experiments.EisenExperiment2
+java -cp target/skypattern-1-jar-with-dependencies.jar org.imt.skypattern.closedsky.experiments.EisenExperiment3
